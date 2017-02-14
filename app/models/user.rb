27e4acd83535bd3f308121c6768fe6ad28f5347a
Jordan Demmouche <jordand75020@hotmail.fr>
2017-02-14 15:09:56 +0100

    add has_many to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
   has_many :motorbikes
+  has_many :rentals
 
   def self.find_for_facebook_oauth(auth)
     user_params = auth.to_h.slice("provider", "uid")
