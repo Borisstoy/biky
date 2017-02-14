f80d6765179f1093467d30bc7eb29a1572f86766
Jean Charles Santi <jean.charles.santi@gmail.com>
2017-02-14 11:09:50 +0100

    upgraded seeds.rb: added one user without MBK // added placeholder 'description' in accordance to new validations in Motorbike Model // added 'puts confirmation' messages for display in console if instances are really persisted in DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,67 +9,85 @@
 
 Motorbike.destroy_all
 User.destroy_all
-user = [
+users = [
+  {
+    email: "titi@gmail.com",
+    password: "654321",
+  },
   {
     email: "toto@gmail.com",
     password: "123456",
   }
 
 ]
-User.create!(user)
+users.each do |u|
+  puts "user '#{u[:email]}' created" if User.create!(u)
+end
 
 motorbikes = [
   {
     user_id: User.last.id,
     model: "Shiver 750",
     brand: "Aprilia",
+    description: "description"
     },
   {
     user_id: User.last.id,
     model: "Dorsoduro 750",
     brand: "Aprilia",
+    description: "description"
     },
   {
     user_id: User.last.id,
     model: "SR Max 300 ie",
     brand: "Aprilia",
+    description: "description"
     },
   {
     user_id: User.last.id,
     model: "SRV 850 ie",
     brand: "Aprilia",
+    description: "description"
     },
   {
     user_id: User.last.id,
     model: "C 650 Sport",
     brand: "BMW",
+    description: "description"
     },
   {
     user_id: User.last.id,
     model: " C 650 GT",
     brand: "BMW",
+    description: "description"
     },
   {
     user_id: User.last.id,
     model: "F 700 GS ",
     brand: "BMW",
+    description: "description"
     },
   {
     user_id: User.last.id,
     model: "F 800 GS",
     brand: "BMW",
+    description: "description"
     },
   {
     user_id: User.last.id,
     model: "F 800 GT",
     brand: "BMW",
+    description: "description"
     },
   {
     user_id: User.last.id,
     model: "Scrambler 800",
     brand: "Ducati",
+    description: "description"
     },
 ]
 
 
-motorbikes.each { |m| Motorbike.create(m) }
+motorbikes.each do |m|
+  puts "motorbike '#{m[:brand]} #{m[:model]}' created" if Motorbike.create!(m)
+end
