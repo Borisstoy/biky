4e6b3d2619cf6ae64519beedb91053ecc36d0154
Jean Charles Santi <jean.charles.santi@gmail.com>
2017-02-17 14:24:47 +0100

    better fb button // condition on avatar photo display in navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def avatar_url(user)
-    user.facebook_picture_url || cl_image_path(user.photo.path)
+    if user.photo.nil?
+      user.facebook_picture_url || image_path("helmet.png")
+    else
+      cl_image_path(user.photo.path)
+    end
   end
 end
